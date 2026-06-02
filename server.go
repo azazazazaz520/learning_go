651014842495f3ed13a6bf391e64742e87a53b16
耀天 闫 <3219412675@qq.com>
2026-06-02 22:52:12 +0800

    feat(server): 添加用户结构体及消息监听功能

--- server.go
+++ server.go
@@ -3,23 +3,61 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+	//在线用户列表
+	OnlineMap map[string]*user
+	mapLock   sync.RWMutex
+
+	//消息广播的channel
+	Message chan string
 }
 
 // 创建一个server的接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*user),
+		Message:   make(chan string),
 	}
 	return server
 }
+
+// 监听Message
+func (this *Server) ListenMessage() {
+	for {
+		msg := <-this.Message
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+// 广播消息的方法
+func (this *Server) BroadCast(user *user, msg string) {
+	sendMsg := "[" + user.Addr + user.Name + msg
+	this.Message <- sendMsg
+}
+
 func (this *Server) Handler(conn net.Conn) {
-	fmt.Println("链接建立成功")
+	user := NewUser(conn)
+	//用户上线 ,将用户加入到onlinemap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	this.BroadCast(user, "已上线")
+
+	//当前handler阻塞
+	select {}
+
 }
 
 func (this *Server) Start() {
